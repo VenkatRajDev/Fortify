b280b9973c9626970e2cb057617f95213ab57df4
venkat <venkatraj.g505@gmail.com>
2025-04-03 23:54:28 +0530

    Upgrade password secure

--- screens/PasswordDetails/PASSDETAIL.jsx
+++ screens/PasswordDetails/PASSDETAIL.jsx
@@ -1,85 +1,139 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Pressable, ScrollView, StyleSheet, Text, View} from 'react-native';
+import React, {useCallback, useMemo, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Light} from '../../Theme/Appearance';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {FontFamily, FontSize} from '../../Theme/Fonts';
 import {SCREEN_HEIGHT, SCREEN_WIDTH} from '../../Theme/Dimension';
-import Animated, { FadeInDown } from 'react-native-reanimated';
+import Animated, {FadeInDown} from 'react-native-reanimated';
 import Heading from './Heading';
-import { Provider as PapperProvider } from 'react-native-paper'
-import { useTheme } from '../../ThemeProvider';
-
+import {Provider as PapperProvider} from 'react-native-paper';
+import {useTheme} from '../../ThemeProvider';
 
 const LightBackground = Light.BGCOLOR;
+const _dotSize = 8;
+
+const SecureDots = () => {
+  return (
+    <View
+      style={{
+        width: _dotSize,
+        height: _dotSize,
+        borderRadius: _dotSize / 2,
+        backgroundColor: Light.TEXT,
+      }}
+    />
+  );
+};
 
 const PASSDETAIL = ({navigation, route}) => {
-  
   const {Details} = route.params;
-  const {THEME,isDarkMode} = useTheme()
+  const {THEME, isDarkMode} = useTheme();
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <SafeAreaView style={[styles.container,{backgroundColor: THEME.BGCOLOR}]}>
+    <SafeAreaView style={[styles.container, {backgroundColor: THEME.BGCOLOR}]}>
       <PapperProvider>
-      <ScrollView
-        contentContainerStyle={{alignItems: 'center'}}
-        showsVerticalScrollIndicator={false}>
-          
-        <Heading navigation={navigation} Details={Details}/>
-
-        <Animated.View
-        style={[styles.ItemContainer,{backgroundColor: THEME.SECOUNDARY2}]}>
-
-          <View style={[styles.accountTitle]}>
-            <View style={[styles.logo]}>
-              <Ionicons
-                name="shield-checkmark"
-                size={FontSize.SUB}
-                color="#4a2b74"
-              />
-            </View>
-            <Text style={[styles.AccountText]}>{Details.account}</Text>
-          </View>
+        <ScrollView
+          contentContainerStyle={{alignItems: 'center'}}
+          showsVerticalScrollIndicator={false}>
+          <Heading navigation={navigation} Details={Details} />
 
           <Animated.View
-          entering={FadeInDown.springify().damping(20).stiffness(30)}
-          style={[styles.passwordContainer]}>
-            <Text style={[styles.placeHolder]}>Username/email address</Text>
-            <View style={[styles.eachItemcontentView]}>
-              <Ionicons name="mail" size={FontSize.SUB} color="#4a2b74" />
-              <Text style={[styles.eachItemText]}>{Details.username}</Text>
+            style={[
+              styles.ItemContainer,
+              {backgroundColor: THEME.SECOUNDARY2},
+            ]}>
+            <View style={[styles.accountTitle]}>
+              <View style={[styles.logo]}>
+                <Ionicons
+                  name="shield-checkmark"
+                  size={FontSize.SUB}
+                  color="#4a2b74"
+                />
+              </View>
+              <Text style={[styles.AccountText]}>{Details.account}</Text>
             </View>
 
-            <Text style={[styles.placeHolder]}>Password</Text>
-            <View style={[styles.eachItemcontentView]}>
-              <Ionicons name="key" size={FontSize.SUB} color="#4a2b74" />
-              <Text style={[styles.eachItemText]}>{Details.password}</Text>
-            </View>
+            <Animated.View
+              entering={FadeInDown.springify().damping(20).stiffness(30)}
+              style={[styles.passwordContainer]}>
+              <Text style={[styles.placeHolder]}>Username/email address</Text>
+              <View style={[styles.eachItemcontentView]}>
+                <Ionicons name="mail" size={FontSize.SUB} color="#4a2b74" />
+                <Text style={[styles.eachItemText]}>{Details.username}</Text>
+              </View>
 
-            <Text style={[styles.placeHolder]}>Connected account</Text>
-            <View style={[styles.eachItemcontentView]}>
-              <Ionicons name="people" size={FontSize.SUB} color="#4a2b74" />
-              <Text style={[styles.eachItemText]}>
-                {Details.connectedAccount}
+              <Text style={[styles.placeHolder]}>Password</Text>
+              <View
+                style={{
+                  ...styles.eachItemcontentView,
+                  justifyContent: 'space-between',
+                }}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: SCREEN_WIDTH * 0.05,
+                  }}>
+                  <Ionicons name="key" size={FontSize.SUB} color="#4a2b74" />
+                  <Text style={[styles.eachItemText]}>
+                    {!showPassword ? (
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          gap: SCREEN_WIDTH * 0.01,
+                        }}>
+                        {Details.password.split('').map((_, index) => {
+                          return <SecureDots key={index.toString()} />;
+                        })}
+                      </View>
+                    ) : (
+                      Details.password
+                    )}
+                  </Text>
+                </View>
+                <Pressable onPress={() => setShowPassword(!showPassword)}>
+                  <Ionicons
+                    name={showPassword ? 'eye' : 'eye-off'}
+                    size={FontSize.SUB}
+                    color="#4a2b74"
+                  />
+                </Pressable>
+              </View>
+
+              <Text style={[styles.placeHolder]}>Connected account</Text>
+              <View style={[styles.eachItemcontentView]}>
+                <Ionicons name="people" size={FontSize.SUB} color="#4a2b74" />
+                <Text style={[styles.eachItemText]}>
+                  {Details.connectedAccount}
+                </Text>
+              </View>
+            </Animated.View>
+
+            <View style={[styles.DetailsContainer]}>
+              <Text
+                style={[
+                  styles.detailHeaing,
+                  {color: isDarkMode ? '#5be264c4' : 'green'},
+                ]}>
+                Details
               </Text>
+
+              <View>
+                <Text style={[styles.placeHolder]}>
+                  Website address/App name
+                </Text>
+                <Text style={[styles.eachItemText]}>{Details.website}</Text>
+              </View>
+
+              <View>
+                <Text style={[styles.placeHolder]}>Notes</Text>
+                <Text style={[styles.eachItemText]}>{Details.notes}</Text>
+              </View>
             </View>
           </Animated.View>
-
-          <View style={[styles.DetailsContainer]}>
-            <Text style={[styles.detailHeaing,{color: isDarkMode ? '#5be264c4' : 'green'}]}>Details</Text>
-
-            <View>
-              <Text style={[styles.placeHolder]}>Website address/App name</Text>
-              <Text style={[styles.eachItemText]}>{Details.website}</Text>
-            </View>
-
-            <View>
-              <Text style={[styles.placeHolder]}>Notes</Text>
-              <Text style={[styles.eachItemText]}>{Details.notes}</Text>
-            </View>
-          </View>
-        </Animated.View>
-      </ScrollView>
+        </ScrollView>
       </PapperProvider>
     </SafeAreaView>
   );
@@ -134,6 +188,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     gap: SCREEN_WIDTH * 0.05,
+    backgroundColor: 'red',
   },
   eachItemText: {
     fontSize: FontSize.SUB - 5,
